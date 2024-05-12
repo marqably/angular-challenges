3807b1607fc7a7b2e118345a336cf78e9d862a36
Bernhard Leitgeb <bernhard.leitgeb@marqably.com>
2024-05-12 12:17:29 +0200

    build: adjust types

--- apps/angular/pipe-easy/src/app/concat-with-hyphen.pipe.ts
+++ apps/angular/pipe-easy/src/app/concat-with-hyphen.pipe.ts
@@ -3,9 +3,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'concatHyphen',
   standalone: true,
+  pure: true,
 })
 export class ConcatWithHyphenPipe implements PipeTransform {
-  transform(value1: unknown, value2: unknown): string {
+  transform(value1: PipeInput, value2: PipeInput): string {
     return `${value1} - ${value2}`;
   }
 }
+
+export type PipeInput = string | number;
